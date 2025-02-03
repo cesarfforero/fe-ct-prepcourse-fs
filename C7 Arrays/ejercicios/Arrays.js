107923ddbb68d44aa22b3e290c2ebb5ac6dc0bd4
César Felipe Forero <cesar23forero@gmail.com>
2025-02-02 21:15:25 -0500

    callbacks

--- C7 Arrays/ejercicios/Arrays.js
+++ C7 Arrays/ejercicios/Arrays.js
@@ -5,4 +5,19 @@ listaDeCompras[3]="Tomates";
 listaDeCompras[1]="Lechuga";
 
 
-console.log(listaDeCompras)
+console.log(listaDeCompras[1]);
+
+function devuelvoUsuario(){
+    return "camilo";
+}
+function devuelvoSaludo(){
+    return "Hola"
+} 
+function saludar(cb1, cb2){
+    return cb1() + " " + cb2();
+}
+
+var resultado = saludar(devuelvoSaludo, devuelvoUsuario);
+
+console.log(resultado);
+
